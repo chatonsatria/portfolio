99dd0fbcbbbfe61cee3dfbf869aa766c7806c508
chatonsatria <martin1998.mcsy@gmail.com>
2022-08-18 00:49:12 +0700

    home feat: fix project search feature

--- components/organisms/home/Projects.tsx
+++ components/organisms/home/Projects.tsx
@@ -1,6 +1,6 @@
 // import FilterDropdown from "../../atom/filters/FilterDropdown";
 import Link from "next/link";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { StoreContext } from "../../../store/store-context";
 import ButtonPrimary from "../../atom/buttons/ButtonPrimary";
 import FilterSearch from "../../atom/filters/FilterSearch";
@@ -8,6 +8,7 @@ import ProjectCard from "../../molecules/ProjectsCard";
 
 export default function Projects() {
   const { realProjects } = useContext(StoreContext);
+  const [realSearch, setRealSearch] = useState("");
   return (
     <section className="flex flex-col gap-y-11 border-b-2 dark:border-[#152C2F] pb-11">
       <p data-aos="fade-right" className="text-center font-semibold text-5xl">
@@ -15,19 +16,29 @@ export default function Projects() {
       </p>
       <div className="flex flex-col gap-y-8">
         <div data-aos="fade-right" className="inline-flex justify-between">
-          <FilterSearch searchValue={(value) => console.log(value)} />
+          <FilterSearch searchValue={(value) => setRealSearch(value)} />
           {/* <FilterDropdown /> */}
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {realProjects.map((data: any, key: number) => (
-            <ProjectCard
-              key={key}
-              imageUrl="/next/developer-light.svg"
-              title={data.title}
-              category={data.category}
-              href={data.slug}
-            />
-          ))}
+          {realProjects
+            .filter((data: any) => {
+              if (!realSearch) return true;
+              if (
+                data.title.toLowerCase().includes(realSearch.toLowerCase()) ||
+                data.category.toLowerCase().includes(realSearch.toLowerCase())
+              ) {
+                return true;
+              }
+            })
+            .map((data: any, key: number) => (
+              <ProjectCard
+                key={key}
+                imageUrl="/next/developer-light.svg"
+                title={data.title}
+                category={data.category}
+                href={data.slug}
+              />
+            ))}
         </div>
         <div data-aos="fade-right" className="flex w-full justify-center">
           <ButtonPrimary buttonText="More Projects" href={"/projects"} />
