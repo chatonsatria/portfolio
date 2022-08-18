21c9aaf7bd317e8d56485e2fcc019556c7b3d29a
chatonsatria <martin1998.mcsy@gmail.com>
2022-08-19 01:32:12 +0700

    atom feat: add button link primary, fix button primary, and fix button dark mode icon to image base

---
+++ components/atom/buttons/ButtonLinkPrimary.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+export default function ButtonLinkPrimary(
+  props: Partial<{
+    buttonText: string;
+    href: string;
+    target_blank: boolean;
+    onClick: () => void;
+  }>
+) {
+  const { buttonText, onClick, href = "", target_blank = true } = props;
+
+  return (
+    <Link href={href}>
+      {target_blank ? (
+        <a target="_blank">
+          <button
+            onClick={onClick}
+            className="flex px-4 h-8 bg-[#2F5D62] hover:bg-[#1f3d41] text-white text-md rounded-md transition-all"
+          >
+            <p className="m-auto">{buttonText}</p>
+          </button>
+        </a>
+      ) : (
+        <button
+          onClick={onClick}
+          className="flex px-4 h-11 bg-[#2F5D62] hover:bg-[#1f3d41] text-white text-md rounded-md transition-all"
+        >
+          <p className="m-auto">{buttonText}</p>
+        </button>
+      )}
+    </Link>
+  );
+}
