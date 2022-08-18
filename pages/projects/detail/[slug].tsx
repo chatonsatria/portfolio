1764f6c50b8a097c718fceba66ecd9c818509399
chatonsatria <martin1998.mcsy@gmail.com>
2022-08-19 01:39:48 +0700

    delete

--- pages/projects/detail/[slug].tsx
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router";
-
-export default function Detail() {
-  const router = useRouter();
-  const { slug } = router.query;
-  console.log("slug", slug);
-  return <div>Detail</div>;
-}
