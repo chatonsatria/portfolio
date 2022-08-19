a8f74b80977e4aff28b0b3dff3e569c8b8523b17
chatonsatria <martin1998.mcsy@gmail.com>
2022-08-20 03:38:23 +0700

    atom feat: button menu delete transition duration

--- components/atom/buttons/ButtonMenu.tsx
+++ components/atom/buttons/ButtonMenu.tsx
@@ -10,17 +10,17 @@ export default function ButtonMenu(props: { onClick: () => void }) {
     <button onClick={onClickHandler} className="md:hidden">
       <div className="flex flex-col gap-y-1">
         <div
-          className={`w-6 h-[3px] rounded-md bg-[#2F5D62] hover:bg-[#1f3d41] dark:bg-white transition-all duration-500 ${
+          className={`w-6 h-[3px] rounded-md bg-[#2F5D62] hover:bg-[#1f3d41] dark:bg-white transition-all ${
             toggle ? "rotate-45 translate-y-[7px]" : ""
           }`}
         />
         <div
-          className={`w-6 h-[3px] rounded-md bg-[#2F5D62] hover:bg-[#1f3d41] dark:bg-white transition-all duration-500 ${
+          className={`w-6 h-[3px] rounded-md bg-[#2F5D62] hover:bg-[#1f3d41] dark:bg-white transition-all ${
             toggle ? "translate-x-3 opacity-0" : ""
           }`}
         />
         <div
-          className={`w-6 h-[3px] rounded-md bg-[#2F5D62] hover:bg-[#1f3d41] dark:bg-white transition-all duration-500 ${
+          className={`w-6 h-[3px] rounded-md bg-[#2F5D62] hover:bg-[#1f3d41] dark:bg-white transition-all ${
             toggle ? "-rotate-45 -translate-y-[7px]" : ""
           }`}
         />
